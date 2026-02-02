bb1ee24987026ab27e63b42ee29bf99cb5df11c1
dinodzambic27 <dinodzambic27@gmail.com>
2026-02-02 07:27:11 +0100

    Add files via upload

---
+++ Zvire.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sigmautulek
+{
+    public class Zvire
+    {
+        public int Id { get; set; }
+        public string Jmeno { get; set; }
+        public string Druh { get; set; }
+        public int Vek { get; set; }
+        public string Pohlavi { get; set; }
+        public DateTime DatumPrijmu { get; set; }
+        public string ZdravotniStav { get; set; }
+        public string Poznamka { get; set; }
+        public bool Adoptovano { get; set; }
+        public DateTime? DatumAdopce { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id,-3} {Jmeno,-10} {Druh,-8} {Vek,-4} {(Adoptovano ? "ANO" : "NE"),-5}";
+        }
+    }
+}
